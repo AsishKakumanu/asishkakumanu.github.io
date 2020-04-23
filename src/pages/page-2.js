fb54d1906d3fff103c29fb7646f4e6f7f26f0c25
Kakumanu Asish <kkmnasish84@gmail.com>
2020-04-22 17:48:02 -0700

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,9 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import HeaderTag from "../components/header.component"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import HeaderTag from "../components/header.component";
 
 const SecondPage = () => (
   <Layout>
@@ -11,10 +11,11 @@ const SecondPage = () => (
     <div className="Hero page2">
       <HeaderTag></HeaderTag>
       <div className="SubHero">
+        Projects page
         <Link to="/">Go back to the homepage</Link>
       </div>
     </div>
   </Layout>
-)
+);
 
-export default SecondPage
+export default SecondPage;
