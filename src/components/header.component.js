af8caf40e4288f1eb950e5802389980201d5ac45
Kakumanu Asish <kkmnasish84@gmail.com>
2020-04-23 19:32:18 -0700

    Modifications to header
    + Added CSS Grid instead of Flexbox
    + Added Header scroll state

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -3,32 +3,48 @@ import { Link } from "gatsby";
 import favicon from "../images/favicon.png";
 
 export default class header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasScrolled: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true });
+    } else {
+      this.setState({ hasScrolled: false });
+    }
+  };
+
   render() {
     return (
       <header>
-        <a href="/" className="alogo">
-          <img className="logo" src={favicon} alt="Logo" />
-        </a>
-
-        <nav>
-          <ul className="nav_links">
-            <li className="home">
-              <Link to="/" className="hover">
-                Home
-              </Link>
-            </li>
-            <li className="projects">
-              <Link to="/page-2/" className="hover">
-                Projects
-              </Link>
-            </li>
-            <li className="contact">
-              <Link to="/page-3/" className="hover">
-                Contact 🚧
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <div
+          className={`headerGroup ${
+            this.state.hasScrolled ? "HeaderScrolled" : "Header"
+          }`}
+        >
+          <a href="/" className="alogo">
+            <img className="logo" src={favicon} alt="Logo" />
+          </a>
+          <Link to="/" className="home">
+            Home
+          </Link>
+          <Link to="/page-2/" className="projects">
+            Projects
+          </Link>
+          <Link to="/page-3/" className="contact">
+            Contact 🚧
+          </Link>
+        </div>
       </header>
     );
   }
