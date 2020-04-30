356477744c7f8d6668cc1f997472edb7363f8051
Kakumanu Asish <kkmnasish84@gmail.com>
2020-04-29 21:19:54 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `Portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
