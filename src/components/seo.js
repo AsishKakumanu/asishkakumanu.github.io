094dea9a1050df12a44d1b017354b75ad07671cb
Kakumanu Asish <kkmnasish84@gmail.com>
2020-04-29 20:50:46 -0700

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -36,6 +36,11 @@ function SEO({ description, lang, meta, image: metaImage, title }) {
       titleTemplate={`${site.siteMetadata.title} | %s`}
       meta={[
         {
+          name: `google-site-verification`,
+          content: `wyY8CNm_ujVIAGvUGT3oGGen5CmIb0VWiyeVAb1I_WU`,
+        },
+
+        {
           name: `description`,
           content: metaDescription,
         },
